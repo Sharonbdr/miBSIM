6c68dd5531de108eb22e9bb17a3cf4a0ca8f94f2
Sharonbdr <142898054+Sharonbdr@users.noreply.github.com>
2023-08-27 03:20:19 +0300

    Update example_script.sh

--- example_script.sh
+++ example_script.sh
@@ -27,7 +27,7 @@ export ROOT_path="/tamir2/sharon/miRNA_models/Code/Features/"
 # Creates models input files and directories, and calculates general features
 cd Code/Integrative/
 matlabr2022b -nojvm -nodesktop -nodisplay -singleCompThread -r "create_Input('$dataset'); base_feat_table('$dataset','$user'); quit;"
-
+echo "done"
 
 # ========================================================================== #
 #                          Biochemical features                              #
