5a0c04ecef5edeec4cf5e14d9f84c1aa2c4ff8eb
Sharonbdr <142898054+Sharonbdr@users.noreply.github.com>
2023-11-30 17:42:16 +0200

    Update example_script.sh

--- example_script.sh
+++ example_script.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-#  Paths and Variables
-mirname = 'mir138'
-mirseq = 'AGCTGGTGTTGTGAATCAGGCCG'
-transcript_file = 'Data/transcripts.txt'
-job_name = 'ex'
-py_env = "C:/Users/sharo/anaconda3/envs/bioPy2"
+# Paths and Variables
+mirname="mir138"
+mirseq="AGCTGGTGTTGTGAATCAGGCCG"
+transcript_file="Data/transcripts.txt"
+job_name="ex"
+py_env="/miniconda3/envs/miBSIM_env" 
 
 
 # Get Biochemical features
 source activate $py_env
 python Code/Features/Biochemical/get_biofeats.py \
---name $mirname \
---mirseq $mirseq \
---transcripts $transcript_file \
---job_name $job_name \                                 
---skip_mir False \
+--name ${mirname} \
+--mirseq ${mirseq} \
+--transcripts ${transcript_file} \
+--job_name ${job_name} \                                 
 --remove_files True \
 
 
 # miBSIM model
-cd ../../miBSIM/
+cd Code/
 matlab -nojvm -nodesktop -nodisplay -singleCompThread -r "predict_miBSIM('$mirname','$mirseq','$transcript_file','$job_name'); quit;"
 
 
